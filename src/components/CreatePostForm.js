7962a8906e44307ee285655240df41f4378c3e0f
erasmocubillos <eerasmocubillos@ucundinamarca.edu.co>
2024-11-07 02:52:06 -0500

    texto

--- src/components/CreatePostForm.js
+++ src/components/CreatePostForm.js
@@ -85,13 +85,13 @@ const CreatePostForm = () => {
                 height: 500,
                 menubar: true,
                 plugins: [
-                  "advlist autolink lists link image charmap print preview anchor",
+                  "code advlist autolink link image lists charmap print preview anchor",
                   "searchreplace visualblocks code fullscreen",
-                  "insertdatetime media table paste code help wordcount",
+                  "insertdatetime media table paste help wordcount",
                 ],
-                toolbar: `undo redo | formatselect | bold italic backcolor | \
-                  alignleft aligncenter alignright alignjustify | \
-                  bullist numlist outdent indent | removeformat | help | image`,
+                toolbar: `code | undo redo | formatselect | bold italic backcolor | 
+                  alignleft aligncenter alignright alignjustify | 
+                  bullist numlist outdent indent | removeformat | help`,
                 images_upload_handler: handleEditorImageUpload,
                 paste_data_images: true,
                 automatic_uploads: true,
